888ddbe7f486b3b1e1dfcf672f2eeed082b8bd60
Lucie-Kin <lchauffo@student.42nice.fr>
2026-02-11 07:17:57 +0100

    fix: homepage anterior version

--- webapp/src/components/homePage/homePage.tsx
+++ webapp/src/components/homePage/homePage.tsx
@@ -218,13 +218,14 @@ export default function HomePage() {
               tournamentMatches={tournament.tournamentMatches}
               currentUser={user ? { id: user.id, name: user.displayName || user.login } : undefined}
             />
-          </div>
-
-          <div className="tournament-wrapper">
-            <TournamentList
-              tournaments={tournament.availableTournaments}
-              onJoin={tournament.joinTournament}
-            />
+            {tournament.availableTournaments.length > 0 && (
+              <div className="tournament-wrapper">
+                <TournamentList
+                  tournaments={tournament.availableTournaments}
+                  onJoin={tournament.joinTournament}
+                />
+              </div>
+            )}
           </div>
 
           <ChatBox 
